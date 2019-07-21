ff5b649cf5bbb8fbf9131b41e891f40de6690c14
Alli <wonga10@gmail.com>
2019-07-20 17:05:17 -0700

    configuration: updated to .net core 2.2, renamed, updated gitignore

--- med-tracker/Controllers/MedTrackerController.cs
+++ med-tracker/Controllers/MedTrackerController.cs
@@ -4,18 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace med_tracker.Controllers
+namespace medtracker.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
         // GET api/values/5
         [HttpGet("{id}")]
@@ -30,16 +24,5 @@ namespace med_tracker.Controllers
         {
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
