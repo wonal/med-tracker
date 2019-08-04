aee06225f3591ebc2745075d2ecac73081459ff7
Alli Wong <wonga10@gmail.com>
2019-08-04 16:10:21 -0700

    adjusted settings for deployment

--- med-tracker/Program.cs
+++ med-tracker/Program.cs
@@ -19,6 +19,8 @@ namespace medtracker
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5002");
+                
     }
 }
