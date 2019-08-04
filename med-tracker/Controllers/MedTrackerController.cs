f5b5d5fe6761594a80a9224b141f9549d51ec843
Alli Wong <wonga10@gmail.com>
2019-08-04 16:10:34 -0700

    started first slash command

--- med-tracker/Controllers/MedTrackerController.cs
+++ med-tracker/Controllers/MedTrackerController.cs
@@ -23,24 +23,33 @@ namespace medtracker.Controllers
 
         // GET api/medtracker
         [HttpGet]
-        public async Task TestMessage()
+        public async Task<IActionResult> TestMessage()
         {
             await client.SendMessage("<token>", "<channel>", "Here's a message");
+            return Ok();
         }
 
         [HttpGet("slackaccess")]
-        public async Task GetAuthTokens(string code)
+        public async Task<IActionResult> GetAuthTokens(string code)
         {
             AuthResponseDTO response = await client.Authorize(code);
             repository.SetCredentials(response.team_id, response);
+            return Ok();
         }
 
-        /*
-        [HttpPost("track")]
-        public async Task TrackMeds(TrackCommandDTO commandDTO)
+        [HttpPost("mt")]
+        public IActionResult TrackMeds(TrackCommandDTO commandDTO)
         {
-
+            /*
+                        int timestamp = int.Parse(Request.Headers["X-Slack-Request-Timestamp"]);
+                        if (DateTimeOffset.Now - DateTimeOffset.FromUnixTimeSeconds(timestamp) > TimeSpan.FromMinutes(5))
+                        {
+                            return BadRequest();
+                        }
+                        string verificationString = Request.Headers["X-Slack-Signature"];
+                        string signature = $"v0:{timestamp}:{Request.Body}";
+            */
+            return Ok("todo");
         }
-        */
     }
 }
