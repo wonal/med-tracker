60a17d6bb993313e47ace7e5e728037f0bfa92be
Alli <wonga10@gmail.com>
2019-09-17 14:53:22 -0700

    fixed averages

--- med-tracker/Infrastructure/DataService.cs
+++ med-tracker/Infrastructure/DataService.cs
@@ -11,9 +11,9 @@ namespace medtracker.Infrastructure
         public static MonthStats CalculateMonthlyStats(IEnumerable<DataDTO> monthlyData)
         {
             int totalHA = monthlyData.Where(x => x.ha_present == true).Count();
-            int totalMaxalt = monthlyData.Where(x => x.num_maxalt > 0).Count();
+            int totalMaxalt = monthlyData.Select(x => x.num_maxalt).Sum();
             decimal avgMaxalt = Decimal.Round(totalMaxalt / new decimal(4), 2);
-            decimal avgAleve = Decimal.Round(monthlyData.Where(x => x.num_aleve > 0).Count() / new decimal(4), 2);
+            decimal avgAleve = Decimal.Round(monthlyData.Select(x => x.num_aleve).Sum() / new decimal(4), 2);
             return new MonthStats { TotalHA = totalHA, TotalMaxalt = totalMaxalt, AvgMaxalt = avgMaxalt, AvgAleve = avgAleve };
         }
 
